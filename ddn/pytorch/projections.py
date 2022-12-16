25610accf8adda9967df059cc1444202a9915224
zhuych01 <2517147593@qq.com>
2022-12-17 00:13:53 +0800

    Update projections.py

--- ddn/pytorch/projections.py
+++ ddn/pytorch/projections.py
@@ -14,6 +14,162 @@
 import torch
 import torch.nn.functional as F
 
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def get_hyperplane_projection(point_to_be_projected_act, 
+                                            weights_act, 
+                                                radius):
+    """Gets the hyperplane projection of a given point.
+    
+    Args:
+        point_to_be_projected_act: Point to be projected with positive components.
+        weights: the weights vector with positive components
+        radius: The radius of weighted l1-ball.
+    Returns:
+        x_sub : The projection point
+    
+    """
+    
+    EPS = torch.finfo(torch.float64).eps
+
+    numerator = torch.inner(weights_act, point_to_be_projected_act) - radius 
+    denominator = torch.inner(weights_act, weights_act) 
+    
+    dual = torch.divide(numerator, denominator + EPS) # compute the dual variable for the weighted l1-ball projection problem
+        
+    x_sub = point_to_be_projected_act - dual * weights_act
+
+    return x_sub,dual
+
+
+def get_weightedl1_ball_projection(point_to_be_projected,
+                                   weights, 
+                                   radius):
+    """Gets the weighted l1 ball projection of given point.
+    
+    Args:
+        point_to_be_projected: Point to be projected.
+        weights: the weights vector.
+        radius: The radius of weighted l1-ball.
+    Returns:
+        x_opt : The projection point.
+    
+    """
+
+    signum = torch.sign(point_to_be_projected)
+    point_to_be_projected_copy = signum * point_to_be_projected
+    
+    
+    act_ind = [True] * point_to_be_projected.shape[0]
+    
+    # The loop of the weight l1-ball projection algorithm
+    while True:
+        # Discarding the zeros 
+        point_to_be_projected_copy_act = point_to_be_projected_copy[act_ind]
+        weights_act = weights[act_ind]
+        
+        # Perform projections in a reduced space R^{|act_ind|}
+        x_sol_hyper, dual = get_hyperplane_projection(point_to_be_projected_copy_act, weights_act, radius)
+        
+        # Update the active index set
+        point_to_be_projected_copy_act = torch.maximum(x_sol_hyper, 0.0)
+
+        point_to_be_projected_copy[act_ind] = point_to_be_projected_copy_act.copy()
+        
+        act_ind = point_to_be_projected_copy > 0
+
+        inact_ind_cardinality = sum(x_sol_hyper < 0)
+        
+        # Check the stopping criteria
+        if inact_ind_cardinality == 0:
+            x_opt = point_to_be_projected_copy * signum
+            break
+
+    # gap = radius -  torch.inner(weights, abs(x_opt))
+    # print(gap)
+    return x_opt, dual
+
+def get_lp_ball_projection(starting_point,
+                    point_to_be_projected, 
+                                        p,
+                                   radius, 
+                                  epsilon = 1e-4,
+                                  Tau = 1.1,
+                                  condition_right=100,
+                                  tol=1e-8,
+                                  MAX_ITER=1000,**kwargs):
+    """Gets the lp ball projection of given point.
+
+    Args:
+    ----------
+    point_to_be_projected: Point to be projected.
+    starting_point: Iterates of IRBP.
+    p: p parameter for lp-ball.
+    radius: The radius of lp-ball.
+    epsilon: Initial value of the smoothing parameter epsilon
+    Tau, condition_right: hyperparameters
+    Returns
+    -------
+    x_final : The projection point 
+    dual : The multiplier
+    Flag_gamma_pos : whether IRBP successfully returens a solution
+    count : The number of iterations
+
+    """
+    if torch.norm(point_to_be_projected, p) ** p <= radius:  
+        return point_to_be_projected
+    
+    # Step 1 and 2 in IRBP.  
+    n = point_to_be_projected.shape[0]
+            
+    signum = torch.sign(point_to_be_projected) 
+    yAbs = signum * point_to_be_projected  # yAbs lives in the positive orthant of R^n
+    
+    lamb = 0.0
+    residual_alpha0 = (1. / n) * torch.norm((yAbs - starting_point) * starting_point - p * lamb * starting_point ** p, 1)
+    residual_beta0 =  abs(torch.norm(starting_point, p) ** p - radius)
+    
+    cnt = 0
+    
+    # The loop of IRBP
+    while True:
+            
+        cnt += 1
+        alpha_res = (1. / n) * torch.norm((yAbs - starting_point) * starting_point - p * lamb * starting_point ** p, 1)
+        beta_res = abs(torch.norm(starting_point, p) ** p - radius)
+        
+        if max(alpha_res, beta_res) < tol * max(max(residual_alpha0, residual_beta0),\
+                                                              1.0) or cnt > MAX_ITER:
+            x_final = signum * starting_point # symmetric property of lp ball
+            break
+        
+            
+        # Step 3 in IRBP. Compute the weights
+        weights = p * 1. / ((torch.abs(starting_point) + epsilon) ** (1 - p) + 1e-12)
+        
+        # Step 4 in IRBP. Solve the subproblem for x^{k+1}
+        gamma_k = radius - torch.norm(abs(starting_point) + epsilon, p) ** p + torch.inner(weights, torch.abs(starting_point))
+            
+        assert gamma_k > 0, "The current Gamma is non-positive"
+         
+        # Subproblem solver : The projection onto weighted l1-ball
+        x_new, lamb = get_weightedl1_ball_projection(yAbs, weights, gamma_k)
+        x_new[torch.isnan(x_new)] = torch.zeros_like(x_new[torch.isnan(x_new)])
+            
+        # Step 5 in IRBP. Set the new relaxation vector epsilon according to the proposed condition
+        condition_left = torch.norm(x_new - starting_point, 2) * torch.norm(torch.sign(x_new - starting_point) * weights, 2) ** Tau
+            
+        if condition_left <= condition_right:
+            theta = torch.minimum(beta_res, 1. / torch.sqrt(cnt)) ** (1. / p)
+            epsilon = theta * epsilon
+            
+        # Step 6 in IRBP. Set k <--- (k+1)
+        starting_point = x_new.copy()
+    return  x_final
+
+
 class Simplex():
     @staticmethod
     def project(v, z = 1.0):
@@ -404,6 +560,45 @@ class EuclideanProjection(torch.nn.Module):
         return 'method={}, radius={}'.format(
             self.method.__name__, self.radius
         )
+
+
+class LpBall():
+    @staticmethod
+    def project(v, z = 1.0,p = 0.8):
+        """ Euclidean projection of a batch of vectors onto an L2-sphere
+
+        Solves:
+            minimise_w 0.5 * || w - v ||_2^2
+            subject to ||w||_p = z
+
+        Arguments:
+            v: (..., n) Torch tensor,
+                batch of n-dimensional vectors to project
+
+            z: float, optional, default: 1.0,
+                radius of the Lp-ball
+
+        Return Values:
+            w: (..., n) Torch tensor,
+                Euclidean projection of v onto the Lp-sphere
+
+        Convergence rate:
+            O(1/sqrt(k))
+        """
+        assert z > 0.0, "z must be strictly positive (%f <= 0)" % z
+        assert p > 0.0 and p < 1.0, "p should between 0.0 and 1.0"
+        # Replace v = 0 with unit vector:
+        w, _ = get_lp_ball_projection(torch.zeros_like(v),v,p,z)
+        # 2. Select v if already inside ball, otherwise select w
+        is_outside = v.norm(p=p, dim=-1, keepdim=True)**p<=z
+        w = torch.where(is_outside, w, v)
+        return w, is_outside
+
+    @staticmethod
+    def gradient(grad_output, output, input, is_outside = None):
+        # Compute vector-Jacobian product (grad_output * Dy(x))
+        # NEED TO BE DONE
+        pass
 
 """ Check gradients
 from torch.autograd import gradcheck
